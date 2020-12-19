a9a608176895fcc82de06d9d4fc6b2811488d80a
Will Fallon <william.fallon@detroitlabs.com>
2020-12-19 01:22:06 -0500

    Added material ui components to clearmapdatapopup, and removed css in favor of material ui styles

--- src/components/views/popups/ClearMapDataPopup.js
+++ src/components/views/popups/ClearMapDataPopup.js
@@ -1,66 +1,63 @@
-/**
- * Provides the interface for the popup which displays
- * when clicking "Clear Map Data" in the Options tab of the map controls.
- */
+import React, { useState } from 'react';
 
-import React from 'react';
-import clsx from 'clsx';
-import Popup from 'reactjs-popup';
-
+import {
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
 import { t } from '~/components/i18n/Localization';
 
-import './ClearMapDataPopup.css';
+const useStyles = makeStyles({
+  dialog: {
+    backgroundColor: '#f0e9e2',
+  },
+});
 
 const ClearMapDataPopup = ({ trigger, onConfirm }) => {
-  // NOTE: className on popup gets overridden with <class>-content, -overlay, and -arrow.
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const classes = useStyles();
   return (
-    <Popup
-      trigger={trigger}
-      className={clsx('popup-clear-map-data')}
-      modal
-      position="center center"
-      closeOnDocumentClick
-      closeOnEscape
-    >
-      {(closePopup) => (
-        <div className={clsx('popup-clear-map-data-container')}>
-          <span className={clsx('popup-clear-map-data-header')}>
-            {t('popup-clear-map-data-title')}
-          </span>
-          <span className={clsx('popup-clear-map-data-content')}>
-            {t('popup-clear-map-data-content')}
-          </span>
-          <div className={clsx('popup-clear-map-data-button-container')}>
-            <div
-              role="button"
-              aria-label={t('popup-cancel')}
-              tabIndex={0}
-              onClick={closePopup}
-              onKeyDown={closePopup}
-              className={clsx('popup-clear-map-data-button', 'popup-clear-map-data-button-cancel')}
-            >
-              {t('popup-cancel')}
-            </div>
-            <div
-              role="button"
-              aria-label={t('popup-confirm')}
-              tabIndex={0}
-              onClick={() => {
-                onConfirm();
-                closePopup();
-              }}
-              onKeyDown={() => {
-                onConfirm();
-                closePopup();
-              }}
-              className={clsx('popup-clear-map-data-button', 'popup-clear-map-data-button-confirm')}
-            >
-              {t('popup-confirm')}
-            </div>
-          </div>
-        </div>
-      )}
-    </Popup>
+    <div>
+      {React.cloneElement(trigger, { onClick: () => setIsDialogOpen(true) })}
+      <Dialog
+        PaperProps={{ className: classes.dialog }}
+        open={isDialogOpen}
+        onClose={() => setIsDialogOpen(false)}
+      >
+        <DialogTitle>{t('popup-clear-map-data-title')}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>{t('popup-clear-map-data-content')}</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button
+            variant="contained"
+            size="large"
+            aria-label={t('popup-cancel')}
+            tabIndex={0}
+            onClick={() => setIsDialogOpen(false)}
+          >
+            {t('popup-cancel')}
+          </Button>
+          <Button
+            variant="contained"
+            size="large"
+            color="primary"
+            aria-label={t('popup-confirm')}
+            tabIndex={0}
+            onClick={() => {
+              onConfirm();
+              setIsDialogOpen(false);
+            }}
+          >
+            {t('popup-confirm')}
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
   );
 };
 
