153b8574f50eb93edb9442734602bba917e3fb9c
EliteMasterEric <ericmyllyoja@gmail.com>
2021-06-07 23:02:45 -0400

    Customize css generation.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { CssBaseline } from '@material-ui/core';
-import { ThemeProvider, StylesProvider } from '@material-ui/core/styles';
+import { ThemeProvider, StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import { appWithTranslation } from 'next-i18next';
 import { AppProps } from 'next/app';
 import React, { FunctionComponent } from 'react';
@@ -11,6 +11,12 @@ import ErrorHandler from 'src/components/views/error/ErrorHandler';
 import FullPageErrorHandler from 'src/components/views/error/FullPageErrorHandler';
 import { NotificationProvider } from 'src/components/views/NotificationProvider';
 
+const generateClassName = createGenerateClassName({
+  disableGlobal: false, // Disable generation of deterministic class names.
+  productionPrefix: 'muicss', // The string used to prefix the class names in production.
+  seed: 'paimon', // String uniquely identifying the generator.
+});
+
 const _app: FunctionComponent<AppProps> = ({ Component, pageProps }) => {
   // TODO: Remove server-side JSS styles.
   // Normally you'd call 'useEffect' to call jssStyles.parentElement.removeChild(jssStyles);
@@ -21,7 +27,7 @@ const _app: FunctionComponent<AppProps> = ({ Component, pageProps }) => {
     <>
       {/* StoreProvider allows hooks and components to access the Redux store. */}
       <StoreProvider store={store}>
-        <StylesProvider injectFirst>
+        <StylesProvider injectFirst generateClassName={generateClassName}>
           {/* ThemeProvider allows for child components to access the Material UI theme. */}
           <ThemeProvider theme={Theme}>
             {/* CSSBaseline injects a basic cascading style sheet for use by Material UI styling. */}
